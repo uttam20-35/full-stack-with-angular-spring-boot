0733066d391d1eaf127634b8942dbe0243145a15
Uttam Thakare <56421970+uttam20-35@users.noreply.github.com>
2025-01-08 13:01:17 +0530

    Update app.componet.ts
    welcome component added

--- frontend/todo/src/app/app.component.ts
+++ frontend/todo/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,WelcomeComponent],
   templateUrl: './app.component.html',
+  // template: '<h1> {{ title }} </h1>',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todo';
+  msg='uttam here'; //Data Binding
 }
