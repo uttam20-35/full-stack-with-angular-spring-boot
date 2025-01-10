295a1562baba1e3ed9ecb4b440cce5159862ce51
Uttam Thakare <56421970+uttam20-35@users.noreply.github.com>
2025-01-10 12:18:22 +0530

    Update Project
    multiple files are updated in project

--- frontend/todo/src/app/app.component.ts
+++ frontend/todo/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
-
+import { LoginComponent } from './login/login.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,WelcomeComponent],
+  imports: [RouterOutlet, LoginComponent, WelcomeComponent],
   templateUrl: './app.component.html',
   // template: '<h1> {{ title }} </h1>',
   styleUrl: './app.component.css'
